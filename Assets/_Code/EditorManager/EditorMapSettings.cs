bf296550750acee454af7b4d1502f2560304fc55
Cat-du-Fromage <florian.duruz@gmail.com>
2021-09-09 10:54:34 +0200

    editor

--- Assets/_Code/EditorManager/EditorMapSettings.cs
+++ Assets/_Code/EditorManager/EditorMapSettings.cs
@@ -9,27 +9,31 @@ using UnityEngine;
 
 namespace KaizerWaldCode
 {
-    [CustomEditor(typeof(ChunkSliceSystem))]
+    [CustomEditor(typeof(UIMapSettings))]
     [CanEditMultipleObjects]
     public class EditorMapSettings : Editor
     {
         [Header("New Game")]
-        [SerializeField] private SerializedProperty newGame;
-        [ConditionalHide(nameof(newGame), true)]
-        [SerializeField] private bool posGroupEnabled = true;
+        private bool newGame;
+        private bool stateChange_NewGame;
+        private SerializedProperty mapSettings;
 
+        private bool posGroupEnabled = true;
+
+        //Save Files
         [Header("Save Files")] 
         public string[] files;
         bool[] pos = new bool[3] { true, true, true };
         
-
+        //Internal Private fields
         private string savesFolder;
         private DirectoryInfo directory;
         private int numSaves;
 
         void Awake()
         {
-            //newGame = serializedObject.FindProperty("newGame"); //Need to be attached to the MonoScript
+            //newGame = serializedObject.FindProperty("newGame");
+            mapSettings = serializedObject.FindProperty("mapSettings");
 
             savesFolder = $"{Application.persistentDataPath}/Save Files/";
             if (!Directory.Exists(savesFolder))
@@ -38,8 +42,6 @@ namespace KaizerWaldCode
             }
 
             DirectoryAndNumSaves(out numSaves, out directory);
-            //directory = new DirectoryInfo(savesFolder);
-            //numSaves = directory.GetFiles("*.dat").Length;
             files = new string[numSaves];
             GetFilesName(savesFolder, ref files, numSaves, directory);
         }
@@ -59,28 +61,53 @@ namespace KaizerWaldCode
 
         public override void OnInspectorGUI()
         {
-            DrawDefaultInspector();
+            //DrawDefaultInspector();
+            UIMapSettings uiSettings = (UIMapSettings)target;
 
-            //newGame.boolValue = EditorGUILayout.Toggle("A Boolean", newGame.boolValue);
-            
-
-            directory = new DirectoryInfo(savesFolder);
-            /*
-            if (numSaves != directory.GetFiles("*.dat").Length)
+            mapSettings = serializedObject.FindProperty("mapSettings");
+            uiSettings.newGame = EditorGUILayout.Toggle("New Game", uiSettings.newGame);
+            if (uiSettings.newGame)
             {
-                numSaves = directory.GetFiles("*.dat").Length;
-                files = new string[numSaves];
-                FilesSavedGames(savesFolder, ref files, numSaves, directory);
+                GUI.enabled = false;
+                EditorGUILayout.PropertyField(mapSettings, new GUIContent("Map Settings"));
+                GUI.enabled = true;
+                EditorGUILayout.PropertyField(mapSettings.FindPropertyRelative("ChunkSize"), new GUIContent("Chunk Size"));
+                EditorGUILayout.PropertyField(mapSettings.FindPropertyRelative("NumChunk"), new GUIContent("Num Chunk"));
+                EditorGUILayout.IntSlider(mapSettings.FindPropertyRelative("PointPerMeter"), 2, 10);
+
+                GUI.enabled = false;
+                int chunkSz = mapSettings.FindPropertyRelative("ChunkSize").intValue;
+                int nbChunk = mapSettings.FindPropertyRelative("NumChunk").intValue;
+                int nbPoint = mapSettings.FindPropertyRelative("PointPerMeter").intValue;
+
+                EditorGUILayout.IntField("Map Size", chunkSz * nbChunk);
+                EditorGUILayout.FloatField("Point Spacing", 1f / (nbPoint - 1f));
+                EditorGUILayout.IntField("ChunkPointPerAxis", (chunkSz * nbPoint) - (chunkSz - 1));
+                EditorGUILayout.IntField("MapPointPerAxis", (nbChunk * chunkSz) * nbPoint - (nbChunk * chunkSz - 1));
+                GUI.enabled = true;
+                //EditorGUILayout.IntField("Chunk Size", uiSettings.mapSettings.ChunkSize);
             }
-            */
-            using (var posGroup = new EditorGUILayout.ToggleGroupScope("Align position", posGroupEnabled))
+            else
             {
-                for (int i = 0; i < numSaves; i++)
+                directory = new DirectoryInfo(savesFolder);
+                using (var posGroup = new EditorGUILayout.ToggleGroupScope("Align position", posGroupEnabled))
                 {
-                    posGroupEnabled = posGroup.enabled;
-                    pos[i] = EditorGUILayout.Toggle(files[i], pos[i]);
+                    for (int i = 0; i < numSaves; i++)
+                    {
+                        posGroupEnabled = posGroup.enabled;
+                        pos[i] = EditorGUILayout.Toggle(files[i], pos[i]);
+                    }
+                }
+
+                if (GUILayout.Button("Generate"))
+                {
+                    Debug.Log(mapSettings.FindPropertyRelative("ChunkSize").intValue);
                 }
             }
+
+            
+
+            serializedObject.ApplyModifiedProperties();
         }
 
         //Found here : https://forum.unity.com/threads/find-all-files-in-folder-and-return-their-path-or-filename.941216/
---
+++ Assets/_Code/EditorManager/UIMapSettings.cs
@@ -0,0 +1,13 @@
+using KaizerWaldCode.TerrainGeneration.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KaizerWaldCode
+{
+    public class UIMapSettings : MonoBehaviour
+    {
+        public bool newGame;
+        public SettingsData mapSettings;
+    }
+}
